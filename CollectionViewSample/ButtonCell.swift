f6a11115683cf7b88d6138c2a3930e2fcf7446fa
shtnkgm <nakagami.shota@gmail.com>
2017-07-24 00:08:36 +0900

    fix

---
+++ CollectionViewSample/ButtonCell.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonCell.swift
+//  CollectionViewSample
+//
+//  Created by Shota Nakagami on 2017/07/23.
+//  Copyright © 2017年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ButtonCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- CollectionViewSample/ViewController.swift
+++ CollectionViewSample/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet fileprivate weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        // Self-Sizingを有効化
+        let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        flowLayout?.estimatedItemSize = CGSize(width: 1, height: 1)
+
+        let nib = UINib(nibName: "ButtonCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "ButtonCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ButtonCell", for: indexPath) as? ButtonCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 100, height: 100)
+//    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
